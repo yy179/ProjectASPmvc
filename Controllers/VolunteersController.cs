957d6a9dc9a6406b2ed324223c87bd996826e064
user <user@WIN-MKQ26SV0D4C>
2024-10-07 21:08:42 +0300

    s

---
+++ Controllers/VolunteersController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLibrary.Services.Interfaces;
+using ProjectLibrary.Models;
+namespace Project.Controllers
+{
+    public class VolunteersController : Controller
+    {
+        private readonly IVolunteerService _volunteerService;
+        public VolunteersController(IVolunteerService volunteerService)
+        {
+            _volunteerService = volunteerService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var volunteers = await _volunteerService.Get();
+            return View(volunteers);
+        }
+        public async Task<IActionResult> Details(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return NotFound();
+            }
+
+            var volunteer = await _volunteerService.GetById(id);
+            if (volunteer == null)
+            {
+                return NotFound();
+            }
+
+            return View(volunteer);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,DateOfBirth,City,Biography")] VolunteerEntity volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                volunteer.Id = Guid.NewGuid();
+                await _volunteerService.Add(volunteer.Id, volunteer.Name, volunteer.DateOfBirth, volunteer.City, volunteer.Biography, new List<OrganizationEntity>(), new List<RequestEntity>());
+                return RedirectToAction(nameof(Index));
+            }
+            return View(volunteer);
+        }
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return NotFound();
+            }
+
+            var volunteer = await _volunteerService.GetById(id);
+            if (volunteer == null)
+            {
+                return NotFound();
+            }
+            return View(volunteer);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,DateOfBirth,City,Biography")] VolunteerEntity volunteer)
+        {
+            if (id != volunteer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _volunteerService.Update(volunteer.Id, volunteer.Name, volunteer.DateOfBirth, volunteer.City, volunteer.Biography, new List<OrganizationEntity>(), new List<RequestEntity>());
+                return RedirectToAction(nameof(Index));
+            }
+            return View(volunteer);
+        }
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return NotFound();
+            }
+
+            var volunteer = await _volunteerService.GetById(id);
+            if (volunteer == null)
+            {
+                return NotFound();
+            }
+
+            return View(volunteer);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _volunteerService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 using ProjectLibrary.Services.Interfaces;
 using ProjectLibrary.Services;
-
+using ProjectLibrary.Repositories.Interfaces;
+using ProjectLibrary.Repositories;
+using ProjectLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ProjectDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IRequestRepository, RequestRepository>();
+builder.Services.AddScoped<IVolunteersRepository, VolunteersRepository>();
+builder.Services.AddScoped<IOrganizationsRepository, OrganizationsRepository>();
+builder.Services.AddScoped<IMilitaryUnitRepository, MilitaryUnitRepository>();
+builder.Services.AddScoped<IContactPersonRepository, ContactPersonRepository>();
+
 builder.Services.AddScoped<IRequestService, RequestService>();
 builder.Services.AddScoped<IVolunteerService, VolunteerService>();
 builder.Services.AddScoped<IOrganizationService, OrganizationService>();
