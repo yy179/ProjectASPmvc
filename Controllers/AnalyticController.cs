655fc995fb0767331b9e7d2efe1d4037e4117fa7
user <user@WIN-MKQ26SV0D4C>
2024-10-18 00:42:04 +0300

    s

---
+++ Controllers/AnalyticController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLibrary.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class AnalyticController : Controller
+    {
+        private readonly IAnalyticsService _analyticsService;
+
+        public AnalyticController(IAnalyticsService analyticsService)
+        {
+            _analyticsService = analyticsService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ProcessedRequestsCount(TimeSpan period)
+        {
+            var count = await _analyticsService.GetProcessedRequestsCount(period);
+            return View("ProcessedRequestsCount", count);
+        }
+
+        public async Task<IActionResult> AverageRequestCompletionTime(TimeSpan period)
+        {
+            var averageTime = await _analyticsService.GetAverageRequestCompletionTime(period);
+            return View("AverageRequestCompletionTime", averageTime);
+        }
+
+        public async Task<IActionResult> TopVolunteers(int topCount = 5)
+        {
+            var volunteers = await _analyticsService.GetTopVolunteers(topCount);
+            return View("TopVolunteers", volunteers);
+        }
+
+        public async Task<IActionResult> VolunteerCount()
+        {
+            var count = await _analyticsService.GetVolunteerCount();
+            return View("VolunteerCount", count);
+        }
+
+        public async Task<IActionResult> OrganizationCount()
+        {
+            var count = await _analyticsService.GetOrganizationCount();
+            return View("OrganizationCount", count);
+        }
+    }
+}
