a7e15a93ffd746ae4e94aa633f554a4f26a36dfd
user <user@WIN-MKQ26SV0D4C>
2024-10-17 19:19:42 +0300

    s

---
+++ Controllers/ContactPersonController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLibrary.Models;
+using ProjectLibrary.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class ContactPersonController : Controller
+    {
+        private readonly IContactPersonService _contactPersonService;
+
+        public ContactPersonController(IContactPersonService contactPersonService)
+        {
+            _contactPersonService = contactPersonService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var contacts = await _contactPersonService.Get();
+            return View(contacts);
+        }
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var contactPerson = await _contactPersonService.GetById(id);
+            if (contactPerson == null)
+                return NotFound();
+            return View(contactPerson);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ContactPersonEntity contactPerson)
+        {
+            if (ModelState.IsValid)
+            {
+                await _contactPersonService.Add(contactPerson.Id, contactPerson.MilitaryUnitId, contactPerson.Name, contactPerson.Surname, contactPerson.DateOfBirth, contactPerson.Address);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactPerson);
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var contactPerson = await _contactPersonService.GetById(id);
+            if (contactPerson == null)
+            {
+                return NotFound();
+            }
+            return View(contactPerson);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, ContactPersonEntity contactPerson)
+        {
+            if (id != contactPerson.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _contactPersonService.Update(contactPerson.Id, contactPerson.MilitaryUnitId, contactPerson.Name, contactPerson.Surname, contactPerson.DateOfBirth, contactPerson.Address);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactPerson);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var contactPerson = await _contactPersonService.GetById(id);
+            if (contactPerson == null)
+            {
+                return NotFound();
+            }
+            return View(contactPerson);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _contactPersonService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
